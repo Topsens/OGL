715b080ca1719317bd99c2d519ff4b67abaf5cd6
sun-xm <sun-xm@hotmail.com>
2026-07-13 11:12:08 +0800

    Update vector io

--- Source/VectorIO.h
+++ Source/VectorIO.h
@@ -11,75 +11,119 @@ namespace VECTORX_NS {
 #define VXNS
 #endif
 
-struct Bracket
+namespace details
 {
-    char bracket;
-};
-
-inline int bracket_index()
-{
-    static const int index = std::ios_base::xalloc();
-    return index;
-}
-
-inline void get_bracket(std::ostream& os, char& open, char& close)
-{
-    switch (os.iword(bracket_index()))
+    struct Bracket
     {
-        case '[':
-        case ']':
-        {
-            open  = '[';
-            close = ']';
-            break;
-        }
+        char bracket;
+    };
 
-        case '(':
-        case ')':
-        {
-            open  = '(';
-            close = ')';
-            break;
-        }
+    struct Indent
+    {
+        int indent;
+    };
 
-        case '<':
-        case '>':
-        {
-            open  = '<';
-            close = '>';
-            break;
-        }
+    struct SingleLine
+    {
+        int singleline;
+    };
 
-        default:
+    inline int bracket_index()
+    {
+        static const int index = std::ios_base::xalloc();
+        return index;
+    }
+
+    inline int indent_index()
+    {
+        static const int index = std::ios_base::xalloc();
+        return index;
+    }
+
+    inline int singleline_index()
+    {
+        static const int index = std::ios_base::xalloc();
+        return index;
+    }
+
+    inline void get_bracket(std::ostream& os, char& opening, char& closing)
+    {
+        switch (os.iword(bracket_index()))
         {
-            open  = '{';
-            close = '}';
-            break;
+            case '[':
+            case ']':
+            {
+                opening = '[';
+                closing = ']';
+                break;
+            }
+
+            case '(':
+            case ')':
+            {
+                opening = '(';
+                closing = ')';
+                break;
+            }
+
+            case '<':
+            case '>':
+            {
+                opening = '<';
+                closing = '>';
+                break;
+            }
+
+            default:
+            {
+                opening = '{';
+                closing = '}';
+                break;
+            }
         }
+    }
+
+    inline int get_indent(std::ostream& os)
+    {
+        auto indent = os.iword(indent_index());
+        return indent >= 1 ? indent - 1 : 4;
     }
 }
 
-inline Bracket bracket(char bracket)
+inline details::Bracket bracket(char bracket)
 {
-    Bracket b;
+    details::Bracket b;
     b.bracket = bracket;
     return b;
 }
 
-inline std::ostream& operator<<(std::ostream& os, const Bracket& bracket)
+inline details::Indent indent(int indent)
 {
-    os.iword(bracket_index()) = bracket.bracket;
+    details::Indent i;
+    i.indent = indent < 1 ? 1 : indent + 1;
+    return i;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const details::Bracket& bracket)
+{
+    os.iword(details::bracket_index()) = bracket.bracket;
+    return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const details::Indent& indent)
+{
+    os.iword(details::indent_index()) = indent.indent;
     return os;
 }
 
 template<size_t Dims, typename Scalar>
 inline std::ostream& operator<<(std::ostream& os, const Vector<Dims, Scalar>& v)
 {
-    char open;
-    char close;
-    get_bracket(os, open, close);
+    char opening;
+    char closing;
+    details::get_bracket(os, opening, closing);
 
-    os << open;
+    os << opening;
 
     if (Dims > 0)
     {
@@ -91,9 +135,55 @@ inline std::ostream& operator<<(std::ostream& os, const Vector<Dims, Scalar>& v)
         }
     }
 
-    return os << close;
+    return os << closing;
 }
 
+template<size_t MRows, size_t MCols, typename Scalar>
+inline std::ostream& operator<<(std::ostream& os, const Matrix<MRows, MCols, Scalar>& m)
+{
+    char opening;
+    char closing;
+    details::get_bracket(os, opening, closing);
+
+    int singleline = os.iword(details::singleline_index());
+
+    os << opening;
+
+    if (!singleline)
+    {
+        os << std::endl;
+        for (int i = 0; i < details::get_indent(os); i++)
+        {
+            os << ' ';
+        }
+    }
+
+    if (MRows > 0)
+    {
+        os << m[0];
+
+        for (size_t i = 1; i < MRows; i++)
+        {
+            os << ',';
+            if (!singleline)
+            {
+                os << std::endl;
+                for (int i = 0; i < details::get_indent(os); i++)
+                {
+                    os << ' ';
+                }
+            }
+            os << m[i];
+        }
+    }
+
+    if (!singleline)
+    {
+        os << std::endl;
+    }
+
+    return os << closing;
+}
 
 #ifdef VECTORX_NS
 }
--- Test/Stream.cpp
+++ Test/Stream.cpp
@@ -12,14 +12,38 @@ int main()
     os << v;
     if ("{1, 2, 3}" != os.str())
     {
+        cout << os.str() << endl;
         return -1;
     }
 
     os.str({});
     os.clear();
+
     os << bracket('[') << v;
     if ("[1, 2, 3]" != os.str())
     {
+        cout << os.str() << endl;
+        return -1;
+    }
+
+    os.str({});
+    os.clear();
+
+    Matrix<2> m = {{1, 2}, {3, 4}};
+    os << m;
+    if ("[\n    [1, 2],\n    [3, 4]\n]" != os.str())
+    {
+        cout << os.str() << endl;
+        return -1;
+    }
+
+    os.str({});
+    os.clear();
+
+    os << indent(2) << bracket('{') << m;
+    if ("{\n  {1, 2},\n  {3, 4}\n}" != os.str())
+    {
+        cout << os.str() << endl;
         return -1;
     }
 
