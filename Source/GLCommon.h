ba1ff7189d7fa5aa757b889af1798e3fba4ddb21
sun-xm <sun-xm@hotmail.com>
2026-09-04 18:35:59 +0800

    Update namespace usage

--- Source/GLCommon.h
+++ Source/GLCommon.h
@@ -4,16 +4,18 @@
 #include <cstdint>
 
 #ifdef VECTORX_NS
-    #define vx VECTORX_NS
+    #define VXNS VECTORX_NS
 #else
-    #define vx
+    #define VXNS
 #endif
 
-typedef vx::Vector<3, uint32_t> Element;
-typedef vx::Vector<3, float>    Vertex;
-typedef vx::Vector<3, float>    Normal;
-typedef vx::Vector<4, float>    Color;
-typedef vx::Vector<2, float>    Coordinate;
+typedef VXNS::Vector<3, uint32_t> Element;
+typedef VXNS::Vector<3, float>    Vertex;
+typedef VXNS::Vector<3, float>    Normal;
+typedef VXNS::Vector<4, float>    Color;
+typedef VXNS::Vector<2, float>    Coordinate;
+
+#undef VXNS
 
 template<typename T, size_t N>
 char(&_ArraySizeHelper(T(&)[N]))[N];
